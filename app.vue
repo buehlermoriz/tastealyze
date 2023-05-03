2ee105ee39ae4c041ba3baf48c03fa4b00016cc9
Selina Negele <selinanegele@MBP-von-Selina.fritz.box>
2023-05-03 18:39:54 +0200

    sidebar

--- app.vue
+++ app.vue
@@ -2,11 +2,12 @@
   <!-- component -->
 <header>
     <nav class="bg-white shadow">
-        <div class="container mx-auto px-6 py-3 ">
+        <div class="container mx-auto px-6 py-5 ">
             <div class="md:flex md:items-center md:justify-between">
                 <div class="flex justify-between items-center">
                     <div class="text-xl font-semibold text-gray-700">
-                        <a href="#" class="text-gray-800 text-xl font-bold hover:text-gray-700 md:text-2xl">tastealyze</a>
+                     <img class="block h-12 w-auto " src="./assets/tastealyze-logo.png" alt="tastealyze" />
+                        
                     </div>
 
                     <!-- Mobile menu button -->
@@ -19,7 +20,7 @@
                     </div>
                 </div>
 
-                <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+                 <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
                 <div class="hidden -mx-4 md:flex md:items-center">
                     <a href="#" class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Allgemeine Informationen</a>
                     <a href="#" class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Rotwein</a>
@@ -39,4 +40,53 @@
         </div> 
     </div>
 </header>
+
+       
+
+       <div x-data="setup()" x-init="$refs.loading.classList.add('hidden');">
+      <div class="flex h-screen antialiased text-gray-900 bg-gray-100 dark:bg-dark dark:text-light">
+       
+
+        <!-- Sidebar -->
+        <div
+          class="fixed inset-y-0 z-10 flex w-80"
+        >
+          <!-- Curvy shape -->
+          <svg
+            class="absolute inset-0 w-full h-full text-white"
+            style="filter: drop-shadow(10px 0 10px #00000030)"
+            preserveAspectRatio="none"
+            viewBox="0 0 309 800"
+            fill="#ffffff"
+          >
+            <path
+              d="M268.487 0H0V800H247.32C207.957 725 207.975 492.294 268.487 367.647C329 243 314.906 53.4314 268.487 0Z"
+            />
+          </svg>
+          <!-- Sidebar content -->
+          <div class="z-10 flex flex-col flex-1">
+            <div class="flex items-center justify-between flex-shrink-0 w-64 p-4">
+              <!-- Logo -->
+              <a href="#">
+                 <img class="block h-12 w-auto " style="padding-left:40px;" src="./assets/tastealyze-logo.png" alt="tastealyze" />
+              </a>
+            </div>
+
+            <nav class="flex flex-col flex-1 w-64 p-4 mt-4" >
+              <a href="#"  class="flex items-center space-x-2" >
+            
+                  <a href="#" class="block mx-12 mt-2 md:mt-0 text-s text-black-700 capitalize hover:text-red-600">Home</a>
+                  <a href="rotwein" class="block mx-12 mt-2 md:mt-0 text-s text-black-700 capitalize hover:text-red-600">Rotwein</a>
+                  <a href="weisswein" class="block mx-12 mt-2 md:mt-0 text-s text-black-700 capitalize hover:text-red-600">Weißwein</a>
+                  <a href="rose" class="block mx-12 mt-2 md:mt-0 text-s text-black-700 capitalize hover:text-red-600">Rosé</a>
+              </a>
+            </nav>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+
+
+
 </template>
