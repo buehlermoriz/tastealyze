ad89ae70a78422c2a9745e6f7d81c31cb43dd159
Nina <ninakrauss@rocketmail.com>
2023-05-03 16:53:12 +0200

    Navbar and Header Tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,42 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  
-  </div>
+  <!-- component -->
+<header>
+    <nav class="bg-white shadow">
+        <div class="container mx-auto px-6 py-3 ">
+            <div class="md:flex md:items-center md:justify-between">
+                <div class="flex justify-between items-center">
+                    <div class="text-xl font-semibold text-gray-700">
+                        <a href="#" class="text-gray-800 text-xl font-bold hover:text-gray-700 md:text-2xl">tastealyze</a>
+                    </div>
+
+                    <!-- Mobile menu button -->
+                    <div class="flex md:hidden">
+                        <button type="button" class="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600" aria-label="toggle menu">
+                            <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">
+                                <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+                <div class="hidden -mx-4 md:flex md:items-center">
+                    <a href="#" class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Allgemeine Informationen</a>
+                    <a href="#" class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Rotwein</a>
+                    <a href="#" class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Weißwein</a>
+                    <a href="#" class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Rosé</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="w-full bg-cover bg-center" style="height:55rem; background-image: url(https://images.unsplash.com/photo-1562601579-599dec564e06?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80);">
+        <div class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
+            <div class="text-center">
+                <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">The taste of <span class="text-red-400">WINE</span></h1>
+              <!--  <button class="mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Start project</button> -->
+            </div> 
+        </div> 
+    </div>
+</header>
 </template>
