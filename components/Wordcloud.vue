73f9ebf69a2f81175174b31d66daeaa50ea4e5c9
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-05-09 17:50:23 +0200

    fix wordcloud

--- components/Wordcloud.vue
+++ components/Wordcloud.vue
@@ -1,14 +1,17 @@
 <template>
-<!-- Wordcloud -->
-<vue-word-cloud
-  style="
-    height: 480px;
-    width: 640px;
-  "
-  :words="[['romance', 19], ['horror', 3], ['fantasy', 7], ['adventure', 3]]"
-  :color="([, weight]) => weight > 10 ? 'DeepPink' : weight > 5 ? 'RoyalBlue' : 'Indigo'"
-  font-family="Roboto"
-/>
+<!-- Wordcloud-->
+<div class="wordcloud">
+    <vue-word-cloud
+      style="
+        height: 480px;
+        width: 640px;
+      "
+      :words=keywords
+      :color="([, weight]) => weight > 0.4 ? 'DeepPink' : weight > 0.2 ? 'RoyalBlue' : 'Indigo'"
+      font-family="Roboto"
+      spacing=0.5
+    />
+</div>
 </template>
 <script>
 import VueWordCloud from 'vuewordcloud';
@@ -16,5 +19,17 @@ export default {
   components: {
     [VueWordCloud.name]: VueWordCloud,
   },
+  props: [
+    'keywords'
+],
 };
 </script>
+<style>
+.wordcloud {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+</style>
