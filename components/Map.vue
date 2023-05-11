c4ea4c778b49770e0225e2e7accaa966a00b3f4d
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-05-11 10:05:21 +0200

    add map

---
+++ components/Map.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="map"></div>
+</template>
+<script>
+import * as d3 from "d3";
+import { geoMercator, geoPath } from "d3-geo";
+import * as topojson from "topojson-client";
+
+export default {
+  mounted() {
+    var width = 900;
+    var height = 600;
+
+    var projection = d3.geoMercator();
+
+    var svg = d3
+      .select("#map")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+    var path = d3.geoPath().projection(projection);
+    var g = svg.append("g");
+    var url = "https://unpkg.com/world-atlas@2.0.2/countries-110m.json";
+
+    var url = "https://unpkg.com/world-atlas@2.0.2/countries-110m.json";
+    fetch(url)
+      .then((response) => response.json())
+      .then((topology) => {
+        g.selectAll("path")
+          .data(topojson.feature(topology, topology.objects.countries).features)
+          .enter()
+          .append("path")
+          .attr("d", path);
+      })
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style>
+#map {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+</style>
