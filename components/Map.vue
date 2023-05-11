f344be8accb205ec4df583d404454730d5554fb8
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-05-11 19:20:50 +0200

    change country names

--- components/Map.vue
+++ components/Map.vue
@@ -21,8 +21,6 @@ export default {
     var path = d3.geoPath().projection(projection);
     var g = svg.append("g");
     var url = "https://unpkg.com/world-atlas@2.0.2/countries-110m.json";
-
-    var url = "https://unpkg.com/world-atlas@2.0.2/countries-110m.json";
     fetch(url)
       .then((response) => response.json())
       .then((topology) => {
@@ -33,22 +31,32 @@ export default {
           .attr("d", path);
       })
       .catch((error) => console.log(error));
+    d3.csv(
+      "https://raw.githubusercontent.com/buehlermoriz/tastealyze/Map/assets/countries.csv"
+    ).then(function (data) {
+      console.log(data);
+      // parse the CSV data into an array of objects
+      data = data.map(function (d) {
+        return {
+          country: d[""],
+          points: +d["points"],
+          price: +d["price"],
+          sentiment: +d["sentiment"],
+        };
+      });
+      // create a map from the data using the country name as the key
+      var countryData = d3.group(data, function (d) {
+        return d.country;
+      });
+
+      g.selectAll("path").style("fill", function (d) {
+        var country = d.properties.name;
+        var points = countryData.get(country)[0].points;
+        return colorScale(points);
+      });
+    });
   },
 };
-d3.csv('../assets/countries.csv',  (data) => {
-  console.log(data);
-  // create a map from the data using the country name as the key
-  var countryData = d3.map(data, function (d) {
-    return d.country;
-  });
-
-  g.selectAll("path").style("fill", function (d) {
-    var country = d.properties.name;
-    var points = +countryData.get(country).points;
-    return colorScale(points);
-  });
-});
-
 </script>
 
 <style>
