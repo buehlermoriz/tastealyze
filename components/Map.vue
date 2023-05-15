e67bb82aaa9e901ef1caa07fd65432bd39fd5d0a
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-05-15 16:52:27 +0200

    radio buttons

--- components/Map.vue
+++ components/Map.vue
@@ -1,11 +1,45 @@
 <template>
-  <div id="map"></div>
+  <div class="grid grid-cols-5 gap-y-1"> 
+  <div class="col-span-4 flex items-center">
+    <div id="map"></div>
+    </div>
+  <div class="col-span-1 flex flex-col justify-center">
+  <div>
+    <div class="radio-group">
+      <input type="radio" id="control_01" name="select" value="points" v-model="selectedValue" checked>
+  <label for="control_01">
+    <h2>Punkte</h2>
+    <p>Diese Darstellung zeigt Ihnen die durchschschnittlichen Punkte, welche Weine aus einem Land erhalten.</p>
+  </label>
+</div>
+<div>
+  <input type="radio" id="control_02" name="select" value="price" v-model="selectedValue">
+  <label for="control_02">
+    <h2>Preis</h2>
+    <p>Diese Darstellung zeigt Ihnen den durchschschnittlichen Preis, welcher für Weine aus den Ländern bezahlt wird.</p>
+  </label>
+</div>
+<div>
+  <input type="radio" id="control_03" name="select" value="sentiment" v-model="selectedValue">
+  <label for="control_03">
+    <h2>Sprache</h2>
+    <p>Diese Darstellung zeigt Ihnen wie positiv oder negativ die Sprache der Reviews zu Weinen aus den Ländern ausfällt.</p>
+  </label>
+    </div>
+  </div>  
+  </div>
+  </div>
 </template>
 
 <script>
 import * as d3 from "d3";
 
 export default {
+  data() {
+    return {
+      selectedValue: "points",
+    };
+  },
   mounted() {
     var width = 1100;
     var height = 800;
@@ -22,9 +56,7 @@ export default {
     const projection = d3
       .geoMercator()
       .scale(160)
-      .center([0, 60])
-
-  
+      .center([0, 60]);
 
     // Load external data and boot
     Promise.all([
@@ -34,37 +66,57 @@ export default {
       d3.csv(
         "https://raw.githubusercontent.com/buehlermoriz/tastealyze/main/assets/countries.csv"
       ),
-    ]).then(function (loadData) {
+    ]).then((loadData) => {
       let topo = loadData[0];
       let data = loadData[1];
 
-      // Create a map from country code to points
-      let pointsByCountry = new Map(data.map((d) => [d.country, +d.points]));
+      const updateMap = () => {
+        // Create a map from country code to selected data value
+        let dataByCountry = new Map(
+          data.map((d) => [d.country, +d[this.selectedValue]])
+        );
 
-      const colorScale = d3
-        .scaleThreshold()
-        .domain([0.2, 0.4, 0.6, 0.8, 1])
-        .range(d3.schemeReds[6]);
+        // set different Color colors depending on user selection
+        let colorScaleDomain;
+        if (this.selectedValue === "points") {
+          colorScaleDomain = [0.2, 0.3, 0.4, 0.5, 0.6];
+        } else if (this.selectedValue === "price") {
+          colorScaleDomain = [10, 30, 50, 70, 90];
+        } else if (this.selectedValue === "sentiment") {
+          colorScaleDomain = [0.2, 0.4, 0.6, 0.8, 1];
+        }
 
-      // Draw the map
-      svg
-        .append("g")
-        .selectAll("path")
-        .data(topo.features)
-        .join("path")
-        // draw each country
-        .attr("d", d3.geoPath().projection(projection))
-        // set the color of each country based on points
-        .attr("fill", function (d) {
-          let points = pointsByCountry.get(d.properties.name) || 0;
-          return colorScale(points);
-        })
-        .attr("stroke", "white")
-        .attr("stroke-width", 0.5);
+        const colorScale = d3
+          .scaleThreshold()
+          .domain(colorScaleDomain)
+          .range(d3.schemeReds[6]);
+
+        // Draw the map
+        svg
+          .append("g")
+          .selectAll("path")
+          .data(topo.features)
+          .join("path")
+          .attr("d", d3.geoPath().projection(projection))
+          .attr("fill", function (d) {
+            let value = dataByCountry.get(d.properties.name) || 0;
+            return colorScale(value);
+          })
+          .attr("stroke", "white")
+          .attr("stroke-width", 0.5);
+      };
+
+      updateMap();
+
+      this.$watch("selectedValue", () => {
+        svg.selectAll("path").remove();
+        updateMap();
+      });
     });
   },
 };
 </script>
+
 <style>
 #map {
   display: flex;
@@ -74,4 +126,48 @@ export default {
   padding-top: 10%;
   padding-bottom: 10%;
 }
+
+input[type="radio"] {
+  display: none;
+}
+
+input[type="radio"]:not(:disabled) ~ label {
+  cursor: pointer;
+}
+
+label {
+  height: 100%;
+  display: block;
+  background: white;
+  border: 2px solid hsla(0, 54%, 18%, 1);
+  border-radius: 20px;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  text-align: center;
+  position: relative;
+}
+
+input[type="radio"]:checked + label {
+  background: hsla(0, 54%, 18%, 1);
+  color: hsla(215, 0%, 100%, 1);
+  box-shadow: 0px 0px 20px hsla(0, 54%, 18%, 0.75);
+}
+
+
+.flex {
+  display: flex;
+}
+
+.items-center {
+  align-items: center;
+}
+
+.flex-col {
+  flex-direction: column;
+}
+
+.justify-center {
+  justify-content: center;
+}
+
 </style>
---
+++ components/MapWrapper.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="map_wrapper">
+        <h1 class="headline-map">Globale Unterschiede <br> in der Weinproduktion</h1>
+        <p id="description">Diese Grafik zeigt globale Unterschiede in der Weinproduktion. Dabei können Sie aus drei Bereichen auswählen. Mit einer Wahl der durchscnittlichen Punkte erhalten Sie eine Visualisierung wie viele Punkte die Tester den Weinen eines Landes auf einer Skala von 0 bis 100 durchscnittlich gegeben haben. Bei der Auswahl des Preises erhalten sie die durchscnittlichen Preise. Zuletzt lassen sich die Ergebnisse einer Sentimentanalyse darstellen. Hierbei wurden die Texte der Produktbewertungen ausgewertet. Je nach dem wie positiv (oder Negativ) eine Produktbeschriebung ist wurde eine Wert zwischen 0 und 1 ermittelt, welcher auf der Karte dargestellt wird.</p>
+        <Map />
+    </div>
+</template>
+<style>
+/* Background Color*/
+.map_wrapper {
+    margin-top: 100px;
+    background-color: #fbfbfbfb;
+    padding-left: 20%;
+    padding-right: 2%;
+}
+#description {
+  height: 100%;
+  display: block;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  position: relative;
+}
+.headline-map {
+    text-align: center;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    line-height: 1;
+    font-size: 4rem;
+    font-weight: bold;
+}
+</style>
