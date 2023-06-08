b315a06683a4aebfb8efa50fbb832d5489f45bf6
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-06-08 15:02:36 +0200

    changes in style

--- components/RadarChart.vue
+++ components/RadarChart.vue
@@ -1,44 +1,54 @@
 <template>
-    <div class="flex justify-center">
-        <canvas id="marksChart" width="200" height="200"></canvas>
+  <div class="flex justify-center">
+    <div class="max-w-2xl">
+      <canvas id="marksChart"></canvas>
     </div>
-    </template>
+  </div>
+</template>
 <script>
-import { Chart, registerables } from 'chart.js';
-import { RadarController, CategoryScale } from 'chart.js';
+import { Chart, registerables } from "chart.js";
+import { RadarController, CategoryScale } from "chart.js";
 Chart.register(...registerables);
 Chart.register(RadarController, CategoryScale);
 
 export default {
-  data(){
-    return{
-        data: { 
-  labels: ["abv","degree","sweet","acidity","body","tannin"],
-  datasets: [{
-    label: "Red",
-    backgroundColor: "rgba(200,0,0,0.2)",
-    data: [14.5,18.0,1,4,5,4]
+  data() {
+    return {
+      data: {
+        labels: ["abv", "degree", "sweet", "acidity", "body", "tannin"],
+        datasets: [
+          {
+            backgroundColor: "rgba(200,0,0,0.1)",
+            data: [14.5, 18.0, 1, 4, 5, 4],
+            pointRadius: 0,
+          },
+          {
+            backgroundColor: "rgba(200,0,0,0.1)",
+            data: [11.5, 10.0, 1, 3, 2, 1],
+            pointRadius: 0,
+          },
+          {
+            backgroundColor: "rgba(200,0,0,0.1)",
+            data: [11, 11.0, 3, 2, 3, 1],
+            pointRadius: 0,
+          },
+        ],
+      },
+    };
   },
-  {
-    label: "White",
-    backgroundColor: "rgba(0,0,200,0.2)",
-    data: [11.5,10.0,1,3,2,1]
-    },
-    {
-    label: "Rose",
-    backgroundColor: "rgba(0,200,0,0.2)",
-    data: [11,11.0,3,2,3,1]
-    },
-    ]}
-  }},
-  mounted(){
+  mounted() {
     var marksCanvas = document.getElementById("marksChart");
     var radarChart = new Chart(marksCanvas, {
-  type: 'radar',
-  data: this.data
-});
-  }
-}
-
-
+      type: "radar",
+      data: this.data,
+      options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
+    });
+  },
+};
 </script>
