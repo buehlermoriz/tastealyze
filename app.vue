b24021ebbb080e36754dabd818ccd4396a616309
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-06-12 12:08:28 +0200

    add story vue

--- app.vue
+++ app.vue
@@ -2,8 +2,11 @@
 <NuxtLayout>
   <head_hero />
   <Introduction />
+  <Story text="hallo hallo" img="redwine" />
   <WineSelector />
+  <Story text="hallo hallo" img="redwine" />
   <sidebar />
+  <Story text="hallo hallo" img="redwine" />
  <clouds />
  <scatter />
 <MapWrapper />
---
+++ components/Story.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="ml-[20%] py-10 bg-gray-500 text-center">
+        {{ text }}
+        <div class="ml-[20%]"></div>
+        <img :src="loaderPath" autoplay loop class="" />
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        },
+        img: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+    loaderPath(){
+      if(process.env.NODE_ENV === "development"){
+        return "/_nuxt/assets/bottles/"+this.img+".png"
+      }
+    //   else{
+    //     if(this.wineType === "Roséwein"){
+    //       return "/_nuxt/Rosewein.f57f7c8b.gif"
+    //     }
+    //     else if (this.wineType === "Rotwein"){
+    //       return "/_nuxt/Rotwein.efba7540.gif"
+    //     }
+    //     else{
+    //       return "/_nuxt/Weißwein.1ba14fd2.gif"
+    //     }
+    //   }
+      
+    }
+  },
+}
+</script>
